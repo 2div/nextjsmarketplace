17fce4144657dbe7d96fe635d709a1a4be41aab2
2div <mhersi819@gmail.com>
2024-02-24 14:18:36 +0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,20 +35,20 @@ export default function Home() {
           .
         </h1>
         <p className="mt-6 text-lg max-w-prose text-muted-foreground">
-          Welcome to DigitalHippo. Every asset on our
+          Welcome to Digital Hippo. Every asset on our
           platform is verified by our team to ensure our
           highest quality standards.
         </p>
-        <div className="flex flex-col sm:flex-rowgap-4 mt-6">
+        <div className="flex flex-col sm:flex-row gap-4 mt-6">
           <Link href='/products' className={buttonVariants()}>Browse Trending</Link>
           <Button variant='ghost' >Our quality promise &rarr;</Button>
         </div>
       </div>
-      {/* TODO: List products */}
     </MaxWidthWrapper>
+    {/* TODO: List products */}
     <section className="border-t border-gray-200 bg-gray-50">
     <MaxWidthWrapper className="py-20">
-      <div className="grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 lg:gap-x-8 lg:gao-y-0 ">
+      <div className="grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-0 ">
         {
           perks.map((perk) => (
             <div key={perk.name} className="text-center md:flex md:items-start md:text-left lg:block lg:text-center">
@@ -59,7 +59,7 @@ export default function Home() {
               </div>
               <div className="mt-6 md:ml-4 md:mt-0 lg:ml-0 lg:mt-6">
                 <h3 className="text-base font-medium text-gray-900">{perk.name}</h3>
-                <p className="mt-3 text-sm text-muted-foreground">{perk.description}</p>
+                <p className="mt-3 text-sm text-muted-foreground ">{perk.description}</p>
               </div>
             </div>
           ))
