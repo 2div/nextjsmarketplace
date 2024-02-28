b951bf6b193c98b78c9536123539dc345dd60a12
2div <mhersi819@gmail.com>
2024-02-28 08:37:08 +0300

    updates

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -47,10 +47,12 @@ const NavItem = ({category,handleOpen,isOpen, isAnyOpen, close}: NavItemProps) =
                       <div className='col-span-4 col-start-1 grid grid-cols-3 gap-x-8'>
                         {
                           category.featured.map((item) => (
-                            <div key={item.name} className='group relative text-base sm:text-sm'>           
-                          
+                            <div key={item.name} className='group relative text-base sm:text-sm'>            
                              <div className='relative aspect-video overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75'>
+                                <Image src={item.imageSrc} alt='product category image' fill className='object-cover object-center'/>
                               </div>
+                              <Link href={item.href} className='mt-6 block font-medium text-gray-900'>{item.name}</Link>
+                              <p className='mt-1' aria-hidden='true' >Shop Now</p>
                             </div>
                           ))
                         }
