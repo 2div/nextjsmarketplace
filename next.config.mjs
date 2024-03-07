435f50cb1b5254d7874ab6e04f59a63d2eb451a7
2div <96177680+2div@users.noreply.github.com>
2024-03-07 14:56:56 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: process.env.NODE_ENV === 'production' ? '/nextjsmarketplace' : ''
+    basePath: process.env.NODE_ENV === 'production' ? '/nextjsmarketplace' : '',
+    images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
