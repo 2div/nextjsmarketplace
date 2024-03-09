f52ee6c0fedf074765f5207be34a3d5abfdb7d7c
2div <96177680+2div@users.noreply.github.com>
2024-03-09 14:03:28 +0300

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,17 +6,17 @@ export const PRODUCT_CATEGORIES = [
             {
               name: 'Editor picks',
               href: `/products?category=ui_kits`,
-              imageSrc: './nav/ui-kits/mixed.jpg',
+              imageSrc: '/nav/ui-kits/mixed.jpg',
             },
             {
               name: 'New Arrivals',
               href: '/products?category=ui_kits&sort=desc',
-              imageSrc: './nav/ui-kits/blue.jpg',
+              imageSrc: '/nav/ui-kits/blue.jpg',
             },
             {
               name: 'Bestsellers',
               href: '/products?category=ui_kits',
-              imageSrc: './nav/ui-kits/purple.jpg',
+              imageSrc: '/nav/ui-kits/purple.jpg',
             },
           ],
         },
@@ -27,17 +27,17 @@ export const PRODUCT_CATEGORIES = [
               {
                 name: 'Favorite Icon Picks',
                 href: `/products?category=icons`,
-                imageSrc: './nav/icons/picks.jpg',
+                imageSrc: '/nav/icons/picks.jpg',
               },
               {
                 name: 'New Arrivals',
                 href: '/products?category=icons&sort=desc',
-                imageSrc: './nav/icons/new.jpg',
+                imageSrc: '/nav/icons/new.jpg',
               },
               {
                 name: 'Bestselling Icons',
                 href: '/products?category=icons',
-                imageSrc: './nav/icons/bestsellers.jpg',
+                imageSrc: '/nav/icons/bestsellers.jpg',
               },
             ],
           },
